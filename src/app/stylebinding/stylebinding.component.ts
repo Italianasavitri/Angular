60e6e1d82fd6e2db644003b83d09fb9cb57d55e5
Italianasavitri <italiana.savitri@gmail.com>
2019-03-08 11:20:58 +0700

    Style Binding

--- src/app/stylebinding/stylebinding.component.ts
+++ src/app/stylebinding/stylebinding.component.ts
@@ -2,10 +2,27 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-stylebinding',
-  templateUrl: './stylebinding.component.html',
-  styleUrls: ['./stylebinding.component.css']
+  template: `
+  	<h2>Welcome {{nama}}!</h2>
+  	<h2 [style.color]="'orange'">Style Binding</h2>
+  	<h2 [style.color]="hasError ? 'red' : 'green'">Style Binding</h2>
+  	<h2 [style.color]="highlightcolor">Style Binding 2</h2>
+  	<h2 [ngStyle]="titleStyles">Style Binding 3</h2>
+  `,
+  styles: []
 })
 export class StylebindingComponent implements OnInit {
+
+  // untuk memberi nama
+  public nama = "Style Binding";
+
+  public hasError = false;
+  public isSpecial = true;
+  public highlightcolor = "orange";
+  public titleStyles = {
+  	color : "blue",
+  	fontStyle : "italic"
+  }
 
   constructor() { }
 
